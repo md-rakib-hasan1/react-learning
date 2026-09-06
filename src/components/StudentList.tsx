7faa03b526a5cab9f9464fba7c161c1fa16f4e11
Md. Rakib Hasan <rakibhasan98840@gmail.com>
2026-09-07 01:55:42 +0600

    add-label

--- src/components/StudentList.tsx
+++ src/components/StudentList.tsx
@@ -8,15 +8,17 @@ const StudentList = () => {
     return (
         <div>
             <h2>Student List</h2>
+            <label htmlFor="studentName">Student Name:</label>
             <input
+                id="studentName"
                 type="text"
                 value={studentName}
                 onChange={(e) => setStudentName(e.target.value)}
             />
 
             <button onClick={() => {
-                // if(studentName.trim()==="") return;
-                setStudents([...students, studentName]);
+                if(studentName.trim()==="") return;
+                setStudents([...students, studentName.trim()]);
                 setStudentName("")
             }}>
                 Add Student</button>

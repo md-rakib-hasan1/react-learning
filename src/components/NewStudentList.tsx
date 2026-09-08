94c75ebcdafd12c1553d33b2a35822477e0989c2
Md. Rakib Hasan <rakibhasan98840@gmail.com>
2026-09-08 07:08:12 +0600

    add-dreaseAge-button

--- src/components/NewStudentList.tsx
+++ src/components/NewStudentList.tsx
@@ -1,6 +1,6 @@
-import {useState} from 'react';
+import { useState } from 'react';
 
-interface Student{
+interface Student {
     id: number;
     name: string;
     age: number;
@@ -9,47 +9,58 @@ interface Student{
 
 const NewStudentList = () => {
 
-    const[students, setStudents]=useState<Student[]>([
+    const [students, setStudents] = useState<Student[]>([
         {
-      id: 1,
-      name: "Rakib",
-      age: 22,
-      department: "CSE",
-    },
-    {
-      id: 2,
-      name: "Hasan",
-      age: 21,
-      department: "EEE",
-    },
+            id: 1,
+            name: "Rakib",
+            age: 22,
+            department: "CSE",
+        },
+        {
+            id: 2,
+            name: "Hasan",
+            age: 21,
+            department: "EEE",
+        },
     ]);
 
 
-    const increaseAge = (id:number)=>{
+    const increaseAge = (id: number) => {
         setStudents(
-            students.map((student)=>
-            student.id === id ? { ...student, age: student.age + 1 }:student 
-        )
+            students.map((student) =>
+                student.id === id ? { ...student, age: student.age + 1 } : student
+            )
         );
+    }
 
+    const decreaseAge = (id: number) => {
+        setStudents(
+            students.map((student) =>
+                student.id === id ? { ...student, age: student.age - 1 } : student
+            )
+        );
     }
 
     return (
         <div>
             <h2>Student List</h2>
 
-            {students.map((student)=>(
+            {students.map((student) => (
                 <div key={student.id}>
                     <p>Name: {student.name}</p>
                     <p>Age: {student.age}</p>
                     <p>Department: {student.department}</p>
 
-                    <button onClick={()=>increaseAge(student.id)}>
+                    <button onClick={() => increaseAge(student.id)}>
                         Increase Age</button>
+
+                    <button onClick={() => decreaseAge(student.id)}>
+                        Decrease Age
+                    </button>
                 </div>
             ))}
 
-            
+
         </div>
     );
 };
